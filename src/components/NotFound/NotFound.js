f884c6631f533ee8d7e75ff9e12b096842b67073
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-04-16 23:49:13 +0600

    header added

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import error from '../../images/error'
 
 const NotFound = () => {
     return (
-        <div>
-            <h2>Not found</h2>
+        <div className='text-center'>
+            <Link to='/'><img src={error} alt="" /></Link>
         </div>
     );
 };
--- src/pages/Shared/Header/Header.js
+++ src/pages/Shared/Header/Header.js
@@ -1,10 +1,34 @@
 import React from 'react';
-import './Header.css'
+import './Header.css';
+import logo from '../../../images/logo.svg'
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import CustomLink from '../../../components/CustomLink/CustomLink';
 
 const Header = () => {
+
     return (
         <div>
-            <h2>Header</h2>
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Container >
+                    <Navbar.Brand as={Link} to="/">
+                        <img src={logo} height="40" alt="" />
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto"></Nav>
+                        <Nav className='nav-style'>
+                            <Nav.Link as={CustomLink} to='/home'>Home</Nav.Link>
+                            <Nav.Link as={CustomLink} to='/blog'>Blog</Nav.Link>
+                            <Nav.Link as={CustomLink} to='/about'>About</Nav.Link>
+                            <NavDropdown className='p-0 text-light' title="Account" id="collasible-nav-dropdown">
+                                <Nav.Link as={CustomLink} to='/signup'>Sign up</Nav.Link>
+                                <Nav.Link as={CustomLink} to='/login'>Log in</Nav.Link>
+                            </NavDropdown>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </div>
     );
 };
