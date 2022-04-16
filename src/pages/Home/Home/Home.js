6a42349bdb236ee49a9f9e87562266d3678f03ed
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-04-16 23:31:38 +0600

    essential components and page create

---
+++ src/pages/Home/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Banner from '../Banner/Banner';
+import './Home.css'
+const Home = () => {
+    return (
+        <div>
+            <h2>This is home page</h2>
+            <Banner />
+        </div>
+    );
+};
+
+export default Home;
