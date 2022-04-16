88001be5560367f402309377a3dcd7e01f6580e5
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-04-17 04:35:32 +0600

    required error handle

--- src/pages/Account/Signup/Signup.js
+++ src/pages/Account/Signup/Signup.js
@@ -1,12 +1,85 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import './Signup.css'
 import { Button, Form } from 'react-bootstrap';
 import SocialAccount from '../SocialAccount/SocialAccount';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../../Firebase/firebase.init'
+import toast from 'react-hot-toast';
 
 const Signup = () => {
     const navigate = useNavigate()
+
+    // 
+    const [yourName, setYourName] = useState({ value: "", error: "" })
+    const [email, setEmail] = useState({ value: "", error: "" })
+    const [password, setPassword] = useState({ value: "", error: "" })
     const [terms, setTerms] = useState(false)
+
+    //react firebase hook 
+    let location = useLocation();
+    let from = location.state?.from?.pathname || "/";
+    
+    const [createUserWithEmailAndPassword] = useCreateUserWithEmailAndPassword(auth);
+
+    //handleName
+    const handleName = event => {
+        const nameValue = event.target.value
+        setYourName(nameValue);
+    }
+    //handle email
+    const handleEmail = event => {
+        const emailValue = event.target.value
+        console.log(emailValue);
+        if (/\S+@\S+\.\S+/.test(emailValue)) {
+            setEmail({ value: emailValue, error: "" });
+        } else {
+            setEmail({ value: "", error: "Please Provide a valid Email" });
+        }
+    }
+
+    //handle password
+    const handlePassword = event => {
+        const passwordValue = event.target.value
+        console.log(passwordValue);
+        if (passwordValue.length < 6) {
+            setPassword({ value: "", error: "Password must be 6 character or more" });
+        }
+        else if (!/(?=.*[A-Z])/.test(passwordValue)) {
+            setPassword({
+                value: "", error: "Password must contain a capital letter",
+            });
+        }
+        else {
+            setPassword({ value: passwordValue, error: "" });
+        }
+    }
+
+
+    // handle Submit
+    const handleSubmit = event => {
+        event.preventDefault()
+
+        if (yourName.value === "") {
+            setYourName({ value: "", error: "Name is required" });
+        }
+        if (email.value === "") {
+            setEmail({ value: "", error: "Email is required" });
+        }
+        if (password.value === "") {
+            setPassword({ value: "", error: "Password is required" });
+        }
+
+        if (email.value && password.value ) {
+            createUserWithEmailAndPassword(email.value, password.value)
+                .then(() => {
+                    toast.success("Welcome to Ema John", { id: "success" });
+                    navigate("/")
+                })
+        }
+    }
+
+
 
     return (
         <div className='login-bg-img'>
@@ -15,17 +88,19 @@ const Signup = () => {
                     <h3 className='text-center pb-3'>Hello There,</h3>
                     <h5 className='text-center pb-3'>Register now to explore more</h5>
 
-                    <Form >
+                    <Form onSubmit={handleSubmit}>
                         <Form.Group className="mb-3 " controlId="formBasicUsername">
-                            <Form.Control type="name" className='py-2' name='name' placeholder="Enter name" required />
+                            <Form.Control type="name" onBlur={handleName} className='py-2' name='name' placeholder="Enter name"  />
                         </Form.Group>
-
+                        {yourName?.error && <p className="text-danger"> {yourName.error}</p>}
                         <Form.Group className="mb-3 " controlId="formBasicEmail">
-                            <Form.Control type="email" className='py-2' name='email' placeholder="Enter email" required />
+                            <Form.Control type="email" onBlur={handleEmail} className='py-2' name='email' placeholder="Enter email"  />
                         </Form.Group>
+                        {email?.error && <p className="text-danger"> {email.error}</p>}
                         <Form.Group className="mb-3" controlId="formBasicPassword">
-                            <Form.Control type="password" className='py-2' name='password' placeholder="Password" required />
+                            <Form.Control type="password" onBlur={handlePassword} className='py-2' name='password' placeholder="Password"  />
                         </Form.Group>
+                        {password?.error && <p className="text-danger"> {password.error}</p>}
                         <div className='d-flex terms-condition   '>
                             <div className=' mb-3 '>
                                 <input onClick={() => setTerms(!terms)} type="checkbox" name="terms" id="terms" />
