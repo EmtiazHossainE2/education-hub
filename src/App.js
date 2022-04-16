0d6be1fe5ab8a26ba3d63c7e7746899e370d5484
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-04-16 23:36:16 +0600

    routes set

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/Home/Home';
+import Blog from './pages/Blog/Blog/Blog';
+import About from './pages/About/About';
+import Login from './pages/Account/Login/Login';
+import Signup from './pages/Account/Signup/Signup';
+import NotFound from './components/NotFound/NotFound';
+import Header from './pages/Shared/Header/Header';
 
 const App = () => {
     return (
         <div>
-            <h2 className='text-center text-danger'>Welcome to edu hub</h2>
+            <Header></Header>
+            <Routes>
+                <Route path='/' element={<Home/>}></Route>
+                <Route path='/home' element={<Home/>}></Route>
+                <Route path='/blog' element={<Blog/>}></Route>
+                <Route path='/about' element={<About/>}></Route>
+                <Route path='/login' element={<Login/>}></Route>
+                <Route path='/signup' element={<Signup/>}></Route>
+                <Route path='*' element={<NotFound/>}></Route>
+            </Routes>
         </div>
     );
 };
