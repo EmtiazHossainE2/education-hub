8867f10d0e621998c092672711f24375afc2d3ef
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-04-17 00:54:06 +0600

    services section added

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import Services from '../Services/Services';
 import './Home.css'
 const Home = () => {
     return (
         <div>
             <Banner />
-            <h2>somthing</h2>
+            <Services/>
         </div>
     );
 };
---
+++ src/pages/Home/Services/Services.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Services.css'
+const Services = () => {
+    
+
+    return (
+        <div className='container'>
+            <div className='text-center'>
+                <h2 className='text-uppercase service-title'>my programs</h2>
+                <p className='fs-4 fst-italic'>Make a difference in the life of a child with a need</p>
+                <div>
+                    
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Services;
