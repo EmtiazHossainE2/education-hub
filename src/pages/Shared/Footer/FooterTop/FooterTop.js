0c361fd05b649a9c1fb592b5f158d5e18796c2ab
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-04-18 20:07:43 +0600

    final check done

--- src/pages/Shared/Footer/FooterTop/FooterTop.js
+++ src/pages/Shared/Footer/FooterTop/FooterTop.js
@@ -6,17 +6,22 @@ const FooterTop = () => {
     return (
         <div className='container  pb-5'>
             <div className="row">
-                <div className="col-md-6 col-sm-12">
+                <div className="col-md-4 col-sm-12">
+                    <div className='mt-5 logo text-center'>
+                        <Link to='/'><img className='w-50' src={logo} alt="" /></Link>
+                    </div>
+                </div>
+                <div className="col-md-8 col-sm-12 ">
                     <div className="row mt-3">
-                        <div className="col-md-6 col-sm-6">
+                        <div className="col-md-6 col-sm-6 d-flex justify-content-center">
                             <div className='important-link'>
                                 <Link to='/about'>About Me</Link>
                                 <Link to='/blog'>Read Blog</Link>
                                 <Link to='/signup'>Sign Up </Link>
-                                <Link  to='/youtube'> My YouTube</Link>
+                                <Link to='/youtube'> My YouTube</Link>
                             </div>
                         </div>
-                        <div className="col-md-6 col-sm-6">
+                        <div className="col-md-6 col-sm-6 d-flex justify-content-center">
                             <div className='important-link'>
                                 <Link to='/help'>get help</Link>
                                 <Link to='/faq'>read FAQs</Link>
@@ -24,11 +29,6 @@ const FooterTop = () => {
                                 <Link to='/contact'>contact me</Link>
                             </div>
                         </div>
-                    </div>
-                </div>
-                <div className="col-md-6 col-sm-12">
-                    <div className='mt-5 logo text-center'>
-                        <Link to='/'><img className='w-25' src={logo} alt=""  /></Link>
                     </div>
                 </div>
             </div>
